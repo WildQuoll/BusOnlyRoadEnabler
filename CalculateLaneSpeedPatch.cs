cda3b9f389e3bcc56d81c88c5fd44450841d72c4
WildQuoll <>
2021-06-22 18:03:54 +0100

    Initial release.

---
+++ CalculateLaneSpeedPatch.cs
@@ -0,0 +1,39 @@
+﻿using CitiesHarmony.API;
+using HarmonyLib;
+using System;
+
+namespace BusOnlyRoadEnabler
+{
+	[HarmonyPatch(typeof(PathFind), "CalculateLaneSpeed")]
+	class IsCategoryValidPatch
+	{
+		///
+		/// Returns TRUE if the road segment has at least 1 "normal" car lane in the given direction.
+		///
+		private static bool HasCarLanes(NetInfo.Direction direction, NetSegment segment)
+        {
+			foreach(var lane in segment.Info.m_lanes)
+            {
+				if(lane.m_laneType == NetInfo.LaneType.Vehicle && 
+				  (lane.m_vehicleType & VehicleInfo.VehicleType.Car) != 0 &&
+			      (lane.m_direction & direction) != 0)
+                {
+					return true;
+                }
+            }
+
+			return false;
+        }
+
+		[HarmonyPostfix]
+		public static void Postfix(byte startOffset, byte endOffset, ref NetSegment segment, NetInfo.Lane laneInfo, bool ___m_transportVehicle, ref float __result)
+		{
+			if (!___m_transportVehicle && // we are a bus
+				laneInfo.m_laneType == NetInfo.LaneType.TransportVehicle && // this is a bus lane
+				!HasCarLanes(laneInfo.m_direction, segment)) // and this is a bus-only road, at least in this direction
+            {
+				__result = 0.02f;
+            }
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using ICities;
+using CitiesHarmony.API;
+
+namespace BusOnlyRoadEnabler
+{
+    public class Mod : IUserMod
+    {
+        public string Name => "Bus-Only Road Enabler";
+        public string Description => "Makes bus-only roads possible.";
+
+        public void OnEnabled()
+        {
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+        public void OnDisabled()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+
+        public static string Identifier = "WQ.BORE/";
+    }
+}
